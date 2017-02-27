18d7014b50ed5804af3209d8160711cb0811ae8c
Greis <mort5240@gmail.com>
2017-02-27 15:37:06 +0100

    Added PlayerManager to contain BL code. Now it is seperate from the view components.

--- ProgressBarExample/ProgressBarExample/MainPage.xaml.cs
+++ ProgressBarExample/ProgressBarExample/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ProgressBarExample.Managers;
 using ProgressBarExample.Models;
 using Xamarin.Forms;
 
@@ -10,39 +11,20 @@ namespace ProgressBarExample
 {
     public partial class MainPage : ContentPage
     {
-        private Player _player;
-        private Enemy _enemy;
+        private PlayerManager _playerMgr;
+        private float _stages = 10;
         public MainPage()
         {
-            _player = new Player() {MaxHealth = 100, CurrentHealth = 100, Damage = 10};
-            _enemy = new Enemy() {MaxHealth = 100, CurrentHealth = 100, Damage = 10};
-
             InitializeComponent();
+            _playerMgr = PlayerManager.GetInstance();    
         }
 
         private void Enemy_OnClicked(object sender, EventArgs e)
         {
-            DealDamage((float)_player.Damage);
+            EnemyHealthBar.Progress = _playerMgr.DealDamage();
+            StageBar.Progress = _playerMgr.MonsterCount / _stages;
         }
 
-        private void DealDamage(float damage)
-        {
-            if (_enemy.CurrentHealth > damage)
-            {
-                _enemy.CurrentHealth -= damage;
-                EnemyHealthBar.Progress = (_enemy.CurrentHealth/_enemy.MaxHealth);
-            }
-            else
-            {
-                NextMonster();
-            }
-        }
 
-        private void NextMonster()
-        {
-            _enemy = new Enemy() {MaxHealth = _enemy.MaxHealth*2, CurrentHealth = _enemy.CurrentHealth*2, Damage = _enemy.Damage*2};
-            EnemyHealthBar.Progress = 1.0;
-            StageBar.Progress += 0.1;
-        }
     }
 }
---
+++ ProgressBarExample/ProgressBarExample/Managers/PlayerManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProgressBarExample.Models;
+
+namespace ProgressBarExample.Managers
+{
+    public class PlayerManager
+    {
+        private static PlayerManager _instance;
+        public int MonsterCount { get; set; }
+
+        private Player _player;
+        private Enemy _enemy;
+
+        private PlayerManager()
+        {
+            _player = new Player() { MaxHealth = 100, CurrentHealth = 100, Damage = 10 };
+            _enemy = new Enemy() { MaxHealth = 100, CurrentHealth = 100, Damage = 10 };
+            MonsterCount = 0;
+        }
+
+        public static PlayerManager GetInstance()
+        {
+            return _instance ?? new PlayerManager();
+        }
+
+        public double DealDamage()
+        {
+            if (_enemy.CurrentHealth > _player.Damage)
+            {
+                _enemy.CurrentHealth -= _player.Damage;
+                return _enemy.CurrentHealth / _enemy.MaxHealth;
+            }
+            else
+            {
+                MonsterCount++;
+                return NextMonster();
+            }        
+        }
+
+
+
+        public double NextMonster()
+        {
+            _enemy = new Enemy() { MaxHealth = _enemy.MaxHealth * 2, CurrentHealth = _enemy.MaxHealth * 2, Damage = _enemy.Damage * 2 };
+            return 1;
+        }
+    }
+}
