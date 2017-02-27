bb1ebf11aeef799ddc28b55cb59e7acbddee900f
Greis <mort5240@gmail.com>
2017-02-27 14:39:35 +0100

    Added enemy model. Progressbars basically work

--- ProgressBarExample/ProgressBarExample/MainPage.xaml.cs
+++ ProgressBarExample/ProgressBarExample/MainPage.xaml.cs
@@ -9,9 +9,33 @@ namespace ProgressBarExample
 {
     public partial class MainPage : ContentPage
     {
+        public float Damage { get; set; }
         public MainPage()
         {
             InitializeComponent();
+            Damage = 0.1f;
+        }
+
+        private void Enemy_OnClicked(object sender, EventArgs e)
+        {
+            DealDamage(Damage);
+        }
+
+        private void DealDamage(float damage)
+        {
+            if (EnemyHealthBar.Progress > 0f)
+            {
+                EnemyHealthBar.Progress -= Damage;
+            }
+            else
+            {
+                NextMonster();
+            }
+        }
+
+        private void NextMonster()
+        {
+            
         }
     }
 }
---
+++ ProgressBarExample/ProgressBarExample/Models/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ProgressBarExample.Models
+{
+    class Enemy
+    {
+        public string Id { get; set; }
+        public double MaxHealth { get; set; }
+        public double CurrentHealth { get; set; }
+        public Image Image { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public double Damage { get; set; }
+    }
+}
